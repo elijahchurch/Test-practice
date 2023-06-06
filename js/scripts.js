6f638a0ac4b3fc9968af417772f9a120e3b42ed4
Jason <elijahchurch@gmail.com>
2023-06-06 13:58:21 -0700

    update JavaScript Co-authored: Orel Butbul orelbutbul13@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,25 @@
+// business logic
+
 function wordCounter(text) {
-    if (text.length === 0) {
+    if (text.trim().length === 0) {
         return 0;
     }
     let wordCount = 0;
     const textArray = text.split(" ");
-    textArray.forEach(function(word) {
-        wordCount++;
+    textArray.forEach(function(element) {
+        if (!Number(element)) {
+            wordCount++; 
+        }        
     });
     return wordCount;
 };
+function numberOfOccurrencesInText(word, text) {
+    const textArray = text.split(" ");
+    let wordCount = 0;
+    textArray.forEach(function(element) {
+      if (element.toLowerCase().includes(word.toLowerCase())) {
+        wordCount++
+      }
+    });
+    return wordCount;
+  };

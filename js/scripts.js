7c2bcd5bdbc07a582ee64f563393600f27697fa4
Jason E. Church <elijahchurch@gmail.com>
2023-06-06 22:01:21 -0700

    create code to follow along with the second portion of the day

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
-// business logic
+// Utility Logic
+
+function isEmpty(testString) {
+  return (testString.trim().length === 0);
+}
+
+// Business Logic
 
 function wordCounter(text) {
-  if (text.trim().length === 0) {
+  if (isEmpty(text)) {
     return 0;
   }
   let wordCount = 0;
@@ -12,28 +18,61 @@ function wordCounter(text) {
     }
   });
   return wordCount;
-};
+}
+
 function numberOfOccurrencesInText(word, text) {
+  if (isEmpty(word)) {
+    return 0;
+  }
   const textArray = text.split(" ");
   let wordCount = 0;
   textArray.forEach(function (element) {
     if (element.toLowerCase().includes(word.toLowerCase())) {
-      wordCount++
+      wordCount++;
     }
   });
   return wordCount;
-};
+}
 
+// UI Logic
 
-function wordRemoving(text) {
-  const textArray = text.split(" ");
-  let resultArray = [];
-  textArray.forEach(function (word) {
-    if (word.toLowerCase().trim() === "zoinks") {
-      console.log(word);
+function boldPassage(word, text) {
+  if (isEmpty(word) || isEmpty(text)) {
+    return null;
+  }
+  const p = document.createElement("p");
+  let textArray = text.split(" ");
+  textArray.forEach(function (element, index) {
+    if (word === element) {
+      const bold = document.createElement("strong");
+      bold.append(element);
+      p.append(bold);
     } else {
-      resultArray.push(word);
+      p.append(element);
     }
-  })
-  return resultArray.join(" ");
-};
+    if (index !== (textArray.length - 1)) {
+      p.append(" ");
+    }
+  });
+  return p;
+}
+
+function handleFormSubmission() {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+  let boldedPassage = boldPassage(word, passage);
+  if (boldedPassage) {
+    document.querySelector("div#bolded-passage").append(boldedPassage);
+  } else {
+    document.querySelector("div#bolded-passage").innerText = null;
+  }
+}
+
+window.addEventListener("load", function () {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
